03df6193d28d13316bb592fbdcb74d355e6675e5
Edem Ahorlu <edemcapp101@gmail.com>
2022-07-12 19:30:00 +0000

    add class course

---
+++ src/main/java/Course.java
@@ -0,0 +1,14 @@
+public class Course {
+    int courseId;
+    String courseName;
+    String location;
+    String instructorName;
+    
+
+    public Course(int courseId, String courseName, String location, String instructorName) {
+        this. courseId = courseId;
+        this.courseName = courseName;
+        this.location = location;
+        this.instructorName = instructorName;
+    }
+}
